02945e26554ca6bdccb8c956e1504ca91161ea5a
EellyZhang <31869183+EellyZhang@users.noreply.github.com>
2017-10-16 16:05:17 -0400

    Add files via upload

---
+++ CubeBehavior.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeBehavior : MonoBehaviour {
+	public string cubeType;
+
+
+	void OnMouseDown()
+	{
+		if (cubeType == "bronze")
+		{Part2GameController.score += 1;
+			Part2GameController.bronzePlayer -= 1;}
+
+		else if (cubeType == "silver")
+		{Part2GameController.score += 10;
+			Part2GameController.silverPlayer -= 1;}
+
+		else if (cubeType == "gold")
+		{Part2GameController.score += 100;
+			Part2GameController.goldPlayer -= 1;}
+
+
+		Destroy(gameObject);
+
+		print ("YOUR SCORE: " + Part2GameController.score);
+
+	}
+}
---
+++ Part2GameController.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Part2GameController : MonoBehaviour
+{
+	public static int bronzePlayer;
+	public static int silverPlayer;
+	public static int goldPlayer;
+	public int miningSpeed;
+	int timeToMine;
+	public GameObject cubePrefab;
+
+	public static int score;
+
+	Vector3 cubePosition;
+	GameObject currentCube;
+
+	// Use this for initialization
+	void Start()
+	{
+
+		bronzePlayer = 0;
+		silverPlayer = 0;
+		goldPlayer = 0;
+		miningSpeed = 3;
+		score = 0;
+		timeToMine = miningSpeed;
+
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+		if (Time.time > timeToMine)
+		{
+			
+			if (bronzePlayer == 2 && silverPlayer == 2 && goldPlayer < 1)
+			{
+				currentCube = Instantiate(cubePrefab, new Vector3(Random.Range(-10, 10), Random.Range(-10, 10), 0), Quaternion.identity);
+				currentCube.GetComponent<Renderer>().material.color = Color.yellow;
+				currentCube.GetComponent<CubeBehavior>().cubeType = "gold";
+
+				goldPlayer += 1;
+			}
+
+
+			else if (bronzePlayer < 4)
+			{
+				currentCube = Instantiate(cubePrefab, new Vector3(Random.Range(-5, 5), Random.Range(-5, 5), 0), Quaternion.identity);
+				currentCube.GetComponent<Renderer>().material.color = Color.red;
+				currentCube.GetComponent<CubeBehavior>().cubeType = "bronze";
+
+				bronzePlayer += 1;
+			}
+
+			else if (bronzePlayer >= 4)
+			{
+				currentCube = Instantiate(cubePrefab, new Vector3(Random.Range(-5, 5), Random.Range(-5, 5), 0), Quaternion.identity);
+				currentCube.GetComponent<Renderer>().material.color = Color.white;
+				currentCube.GetComponent<CubeBehavior>().cubeType = "silver";
+
+				silverPlayer += 1;
+			}
+
+			print("Bronze: " + bronzePlayer + "Silver: " + silverPlayer + "Gold: " + goldPlayer);
+
+			timeToMine += miningSpeed;
+		}
+	}
+}
+
+
+
+
+// there is no way to spawn a gold ore. Is the gold part for Final? 
